6b79be565e9650d88d5cb0183274a135960868c1
Yash Gautam <156326565+Yash2031v@users.noreply.github.com>
2026-08-19 09:57:55 +0530

    push krdo

--- main/main.js
+++ main/main.js
@@ -1,1 +1,30 @@
-console.log("Hello, World!");
+// let a=10;
+// console.log(typeof (a));
+// let name="vaibhavii";
+// let n;
+// console.log(n);
+// console.log(typeof(n));
+
+// let y=null;
+// console.log(y);
+// const x={
+//     name:"vaibhavii"
+
+// }
+// console.log(x);
+// let a=20;
+// if(a>18){
+//     console.log("eligible for vote");
+// }
+// else{
+//     console.log("not eligible for vote");
+// }
+let data = "a"
+switch (data) {
+    case "a":
+        console.log("a is selected");
+        break;
+    default:
+        console.log("not selected");
+        break;
+}
